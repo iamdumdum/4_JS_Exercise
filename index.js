7d10bbf7b3f09ee9e00a0f501d039cf3210b473b
iamdumdum <Trevorjiang@protonmail.com>
2020-12-16 21:51:48 +0000

    done

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 // only change code below this line
 
-var sum = 10 + 0; //change 0 so that the sum is 20
+var sum = 10 + 10; //change 0 so that the sum is 20
 
-var difference = 15 - 0; //change 0 so that the difference is 5
+var difference = 15 - 10; //change 0 so that the difference is 5
 
-var product = 8 * 0; //change 0 so that the product is 16
+var product = 8 * 2; //change 0 so that the product is 16
 
-var quotient = 6 / 0; //change 0 so that quotient is 2
+var quotient = 6 / 3; //change 0 so that quotient is 2
 
 // only change code above this line
 
